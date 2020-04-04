9d1d2249efce4bb1d47e9a832c46f3221ca094de
brutal-snezhok <czyrulik97@mail.ru>
2020-04-04 11:34:36 +0300

    added repo filters

--- src/main/java/com/tsyrulik/topjava/util/MealsUtil.java
+++ src/main/java/com/tsyrulik/topjava/util/MealsUtil.java
@@ -32,7 +32,7 @@ public class MealsUtil {
     }
 
     public static List<MealTo> getFilteredTos(Collection<Meal> meals, int caloriesPerDay, LocalTime startTime, LocalTime endTime) {
-        return filteredByStreams(meals, caloriesPerDay, meal -> DateTimeUtil.isBetweenHalfOpen(meal.getDateTime().toLocalTime(), startTime, endTime));
+        return filteredByStreams(meals, caloriesPerDay, meal -> Util.isBetweenHalfOpen(meal.getDateTime().toLocalTime(), startTime, endTime));
 
     }
 
