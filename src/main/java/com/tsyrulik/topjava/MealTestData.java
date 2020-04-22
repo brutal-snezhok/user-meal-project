1596c2b68c7316319670abb2e9bff8c6d1a6a217
brutal-snezhok <czyrulik97@mail.ru>
2020-04-22 11:46:34 +0300

    added entity meal with validation

--- src/main/java/com/tsyrulik/topjava/MealTestData.java
+++ src/main/java/com/tsyrulik/topjava/MealTestData.java
@@ -9,7 +9,7 @@ import static com.tsyrulik.topjava.model.AbstractBaseEntity.START_SEQ;
 import static java.time.LocalDateTime.of;
 
 public class MealTestData {
-    public static TestMatcher<Meal> MEAL_MATCHER = TestMatcher.usingFieldsComparator();
+    public static TestMatcher<Meal> MEAL_MATCHER = TestMatcher.usingFieldsComparator("user");
 
     public static final int MEAL1_ID = START_SEQ + 2;
     public static final int ADMIN_MEAL_ID = START_SEQ + 9;
