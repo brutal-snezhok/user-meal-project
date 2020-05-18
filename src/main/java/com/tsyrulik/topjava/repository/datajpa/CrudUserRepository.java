11d5661e25591d1fca3b68797236d82cfeae0429
brutal-snezhok <czyrulik97@mail.ru>
2020-05-18 23:10:07 +0300

    added graph & batch size

--- src/main/java/com/tsyrulik/topjava/repository/datajpa/CrudUserRepository.java
+++ src/main/java/com/tsyrulik/topjava/repository/datajpa/CrudUserRepository.java
@@ -6,17 +6,18 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.EntityGraph;
 
 @Transactional(readOnly = true)
 public interface CrudUserRepository extends JpaRepository<User, Integer> {
     @Transactional
     @Modifying
-//    @Query(name = User.DELETE)
     @Query("DELETE FROM User u WHERE u.id=:id")
     int delete(@Param("id") int id);
 
     User getByEmail(String email);
 
-    @Query("SELECT u FROM User u LEFT JOIN FETCH u.meals WHERE u.id = ?1")
+    @EntityGraph(attributePaths = {"meals", "roles"})
+    @Query("SELECT u FROM User u WHERE u.id=?1")
     User getWithMeals(int id);
 }
