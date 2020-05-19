c1c9bc6cea663e4c2b007fff2a7701c6f99d71c3
brutal-snezhok <czyrulik97@mail.ru>
2020-05-19 18:53:33 +0300

    added spring web mvc

---
+++ src/main/java/com/tsyrulik/topjava/web/RootController.java
@@ -0,0 +1,35 @@
+package com.tsyrulik.topjava.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.tsyrulik.topjava.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class RootController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
+
+    @GetMapping("/users")
+    public String getUsers(Model model) {
+        model.addAttribute("users", service.getAll());
+        return "users";
+    }
+
+    @PostMapping("/users")
+    public String setUser(HttpServletRequest request) {
+        int userId = Integer.parseInt(request.getParameter("userId"));
+        SecurityUtil.setAuthUserId(userId);
+        return "redirect:meals";
+    }
+}
