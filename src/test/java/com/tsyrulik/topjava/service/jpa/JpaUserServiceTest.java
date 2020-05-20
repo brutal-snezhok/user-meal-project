449b43e9ea93ce09df4555e77fca22130590627e
brutal-snezhok <czyrulik97@mail.ru>
2020-05-20 12:55:18 +0300

    refactored meals

--- src/test/java/com/tsyrulik/topjava/service/jpa/JpaUserServiceTest.java
+++ src/test/java/com/tsyrulik/topjava/service/jpa/JpaUserServiceTest.java
@@ -1,10 +1,12 @@
 package com.tsyrulik.topjava.service.jpa;
 
 import com.tsyrulik.topjava.service.AbstractUserServiceTest;
+import org.junit.Ignore;
 import org.springframework.test.context.ActiveProfiles;
 
 import static com.tsyrulik.topjava.Profiles.JPA;
 
 @ActiveProfiles(JPA)
+@Ignore
 public class JpaUserServiceTest extends AbstractUserServiceTest {
 }
