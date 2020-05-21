b1278a6b84fc7e09b0268034f954aa782d484e13
brutal-snezhok <czyrulik97@mail.ru>
2020-05-21 14:26:31 +0300

    added rest test jackson

---
+++ src/test/java/com/tsyrulik/topjava/web/user/AdminRestControllerTest.java
@@ -0,0 +1,25 @@
+package com.tsyrulik.topjava.web.user;
+
+import com.tsyrulik.topjava.web.AbstractControllerTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static com.tsyrulik.topjava.UserTestData.ADMIN_ID;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class AdminRestControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = AdminRestController.REST_URL + '/';
+
+    @Test
+    void get() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(REST_URL + ADMIN_ID))
+                .andExpect(status().isOk())
+                .andDo(print())
+                // https://jira.spring.io/browse/SPR-14472
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+}
