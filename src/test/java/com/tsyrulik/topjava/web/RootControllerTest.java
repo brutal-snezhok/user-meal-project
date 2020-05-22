1d18f1da5ad4b4d23990d473ef3dcd044b60021f
brutal-snezhok <czyrulik97@mail.ru>
2020-05-22 13:12:30 +0300

    added test to rootController

--- src/test/java/com/tsyrulik/topjava/web/RootControllerTest.java
+++ src/test/java/com/tsyrulik/topjava/web/RootControllerTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static com.tsyrulik.topjava.MealTestData.MEALS;
 import static com.tsyrulik.topjava.UserTestData.*;
+import static com.tsyrulik.topjava.util.MealsUtil.getTos;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -29,5 +31,15 @@ class RootControllerTest extends AbstractControllerTest {
                             }
                         }
                 ));
+    }
+
+    @Test
+    void getMeals() throws Exception {
+        perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals", getTos(MEALS, SecurityUtil.authUserCaloriesPerDay())));
     }
 }
