5dc34c9c277ae13b81225c40c2169fd27c01f0cb
brutal-snezhok <czyrulik97@mail.ru>
2020-05-27 07:51:26 +0300

    added password encoding

--- src/main/java/com/tsyrulik/topjava/util/UserUtil.java
+++ src/main/java/com/tsyrulik/topjava/util/UserUtil.java
@@ -1,5 +1,7 @@
 package com.tsyrulik.topjava.util;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.StringUtils;
 import com.tsyrulik.topjava.model.Role;
 import com.tsyrulik.topjava.model.User;
 import com.tsyrulik.topjava.to.UserTo;
@@ -21,6 +23,13 @@ public class UserUtil {
         user.setEmail(userTo.getEmail().toLowerCase());
         user.setCaloriesPerDay(userTo.getCaloriesPerDay());
         user.setPassword(userTo.getPassword());
+        return user;
+    }
+
+    public static User prepareToSave(User user, PasswordEncoder passwordEncoder) {
+        String password = user.getPassword();
+        user.setPassword(StringUtils.hasText(password) ? passwordEncoder.encode(password) : password);
+        user.setEmail(user.getEmail().toLowerCase());
         return user;
     }
 }
